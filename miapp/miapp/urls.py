7d36b2087857350057bfd8fb3100ce3f63218b87
Lucas Sorzio <sorziolucas4@gmail.com>
2024-12-19 22:26:49 -0300

    creando la app
    creando la app

--- miapp/miapp/urls.py
+++ miapp/miapp/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import view
+from django.urls import path ,include
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-
-    path('members/', view.members, name='members'),
+    path('tienda/', include('tienda.urls')),
 
 
 ]
--- miapp/miapp/view.py
+++
@@ -1,8 +0,0 @@
-
-
-
-from django.http import HttpResponse
-
-
-def members(request):
-    return HttpResponse("Hello world!")
